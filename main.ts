7c00871ae8c229881601445dc6243e81eaf5943b
Joey Wunderlich <jwunderl@users.noreply.github.com>
2019-09-19 17:17:48 -0700

    clean up a bit, no nested on game updates (#2)
    * clean up a bit, no nested on game updates
    
    * consistency

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 namespace storyboard {
     function bootMicrosoftLogo(done: () => void) {
-        const font = image.font8;
-        const f = image.doubledFont(font);
+        const TARGET_POSITION_KEY = "target_position"
+        const text = "Microsoft";
+        const f = image.doubledFont(image.font8);
         const bg = 0x6;
         const tc = 0x1;
         const colorOffset = 2;
@@ -21,15 +22,14 @@ namespace storyboard {
 
         pause(300);
 
-        const text = "Microsoft";
-
         const textSprite = sprites.create(
             image.create(text.length() * f.charWidth, f.charHeight),
             Text
         );
-        textSprite.image.print(text, 0, 0, tc, f)
+        textSprite.image.print(text, 0, 0, tc, f);
         textSprite.x += 10;
         textSprite.z = -1;
+        textSprite.setFlag(SpriteFlag.Ghost, true);
 
         const overlaySprite = sprites.create(textSprite.image.clone(), Overlay);
         overlaySprite.image.replace(tc, bg);
@@ -48,25 +48,11 @@ namespace storyboard {
             const yDiff = s.width >> 1;
             s.y += i < 2 ? -(yDiff + 1) : yDiff;
             s.x += (i % 2) * (s.height + 1) + (screen.width >> 1);
-            s.data = textSprite.left - 3 - (((i + 1) % 2) * 9);
+            s.data[TARGET_POSITION_KEY] = textSprite.left - 3 - (((i + 1) % 2) * 9);
             logoSprites.push(s);
         }
 
-        control.runInParallel(() => {
-            logoSprites[2].vx = -50;
-            logoSprites[2].ax = -1500;
-            pause(20);
-            logoSprites[0].vx = -50;
-            logoSprites[0].ax = -1500;
-            pause(20);
-            logoSprites[3].vx = -50;
-            logoSprites[3].ax = -1500;
-            pause(20);
-            logoSprites[1].vx = -50;
-            logoSprites[1].ax = -1500;
-            pause(20);
-        })
-
+        // overlap to clear overlay
         sprites.onOverlap(Logo, Overlay, (s: Sprite, os: Sprite) => {
             os.image.fillRect(
                 s.left - os.left,
@@ -76,59 +62,65 @@ namespace storyboard {
                 0
             );
         });
-
-        let isDoneEntering = false;
-        game.onUpdate(function () {
-            let justFinished = false;
-            if (!isDoneEntering) {
-                justFinished = logoSprites
-                    .map(s => {
-                        if (s.right <= s.data) {
-                            s.ax = 0;
-                            s.vx = 0;
-                            s.right = s.data;
-                        }
-                        return s.right == s.data;
-                    })
-                    .every(b => b);
-            }
-
-            if (justFinished) {
-                isDoneEntering = true;
-                overlaySprite.destroy();
-                control.runInParallel(() => {
-                    pause(1000);
-                    logoSprites.forEach(s => s.ax = 500);
-                    textSprite.ax = -500;
-                })
-
-                let isDoneExiting = false;
-                game.onUpdate(function () {
-                    if (!isDoneExiting && logoSprites[3].left >= screen.width >> 1) {
-                        isDoneExiting = true;
-                        logoSprites.forEach(s => {
-                            s.ax = 0
-                            s.vx = 0
-                        });
-                        control.runInParallel(function () {
-                            pause(750);
-                            logoSprites.forEach(s => s.destroy());
-                            pause(250);
-                            done();
-                        });
-                    }
-                });
-                sprites.onOverlap(Logo, Text, (s: Sprite, os: Sprite) => {
-                    os.image.fillRect(
-                        0,
-                        0,
-                        s.right - os.left, // make sure everything behind this is handled
-                        os.height,
-                        0
-                    );
-                });
-            }
+        // overlap to clear text
+        sprites.onOverlap(Logo, Text, (s: Sprite, os: Sprite) => {
+            os.image.fillRect(
+                0,
+                0,
+                s.right - os.left, // make sure everything behind this is handled
+                os.height,
+                0
+            );
         });
+
+        let allSpritesInPosition = false;
+        game.onUpdate(() => {
+            allSpritesInPosition = logoSprites
+                .map(s => {
+                    const targetPosition = s.data[TARGET_POSITION_KEY];
+                    if (s.right < targetPosition) {
+                        s.ax = 0;
+                        s.vx = 0;
+                        s.right = targetPosition;
+                    }
+                    return s.right <= targetPosition;
+                })
+                .every(b => b);
+        });
+
+        logoSprites[2].vx = -50;
+        logoSprites[2].ax = -1500;
+        pause(20);
+        logoSprites[0].vx = -50;
+        logoSprites[0].ax = -1500;
+        pause(20);
+        logoSprites[3].vx = -50;
+        logoSprites[3].ax = -1500;
+        pause(20);
+        logoSprites[1].vx = -50;
+        logoSprites[1].ax = -1500;
+        pause(20);
+
+        pauseUntil(() => allSpritesInPosition);
+
+        overlaySprite.destroy();
+
+        pause(1000);
+        logoSprites.forEach(s => s.ax = 500);
+        textSprite.ax = -500;
+        textSprite.setFlag(SpriteFlag.Ghost, false);
+
+        pauseUntil(() => logoSprites[3].left >= screen.width >> 1);
+
+        logoSprites.forEach(s => {
+            s.ax = 0
+            s.vx = 0
+        });
+
+        pause(750);
+        logoSprites.forEach(s => s.destroy());
+        pause(250);
+        done();
     }
 
     /**
